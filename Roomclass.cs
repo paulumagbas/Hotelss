2ee0f4290de1b60e853691aa5949f417b11ca4bf
paulumagbas <129728460+paulumagbas@users.noreply.github.com>
2023-07-13 10:03:15 +0800

    Create Roomclass.cs

---
+++ Roomclass.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace John Paulo
+{
+    internal class Room Class
+    {
+        public string room number;
+        public string room type;
+        public int price;
+    }
+}
