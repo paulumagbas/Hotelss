788e922ddde9226b18763b775f75ab35af2f3a89
paulumagbas <129728460+paulumagbas@users.noreply.github.com>
2023-07-13 10:02:32 +0800

    Create Hotel.cs

---
+++ Hotel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace John Paulo
+{ 
+    internal class Hotel
+    {
+      public string name;
+      public string location;
+    }
+}
